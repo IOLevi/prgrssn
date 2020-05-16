8278c9398a28f11fdada3b7a087c1556c403cc09
IOLevi <337@holbertonschool.com>
2020-05-15 20:56:47 -0400

    replace header with own component; replace favicon

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import Head from 'next/head'
-
+import Header from '../components/header';
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+
+      <Header/>
 
       <main>
         <h1 className="title">
