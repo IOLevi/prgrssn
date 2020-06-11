c8c73359b9fc336e0246b053282af2cf9c1095e3
IOLevi <337@holbertonschool.com>
2020-06-10 18:11:31 -0700

    add support for component level css

--- components/header.js
+++ components/header.js
@@ -1,10 +1,13 @@
 import Head from 'next/head';
+import styles from './header.less';
 
 export default function Header() {
     return (
-        <Head>
-            <title>Prgssn Records</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <>
+            <Head>
+                <title>prgrssnrecords</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </>
     )
 }
---
+++ components/topnav.js
@@ -0,0 +1,10 @@
+import styles from './topnav.less';
+
+export default function Topnav() {
+
+    return (
+        <div className={styles.topnav}>
+            This is top nav
+        </div>
+    )
+}
