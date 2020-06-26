84ca3d9d441e7acf56af36d1d6e33db6650541a5
IOLevi <evan@education.com>
2020-06-25 22:01:13 -0700

    noindex site for now

--- components/header.js
+++ components/header.js
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <>
             <Head>
+                <meta name="robots" content="noindex"/>
                 <title>prgrssnrecords</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
