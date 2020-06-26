97fd19c2588f4651edc5b0ba809a9fb0c7b36db2
IOLevi <evan@education.com>
2020-06-25 20:50:01 -0700

    fixing mobile and adding social icons to topnav

--- components/topnav.js
+++ components/topnav.js
@@ -3,13 +3,14 @@ import styles from './topnav.module.css';
 export default function Topnav() {
 
     return (
-        <div className={styles.topnavContainer}>
-            <div className={styles.topnav}>
-                <span className={styles.prgrssnTag}>
-                    prgrssnrecords
-                </span>
+        <div className={styles.topnav}>
+            <div className={styles.prgrssnTag}>
+                prgrssnrecords
+            </div>
+            <div className={styles.icons}>
+                <a href={'https://twitter.com/prgrssnrecords'}><img src={'/twitter.svg'}/></a>
+                <a href={'https://www.facebook.com/prgrssnrecords/'}><img src={'/facebook.svg'}/></a>
             </div>
         </div>
-
     )
 }
